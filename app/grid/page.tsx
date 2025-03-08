72da56989adb0827a4d0de7ad69c69a27d895ce7
Zehua Chen <zehua-chen@outlook.com>
2025-03-07 16:59:34 -0800

    [chore] Add todos.

---
+++ app/grid/page.tsx
@@ -0,0 +1,10 @@
+/**
+ * Demonstrate how to implement grid
+ * - TODO: CSS background + linear gradient
+ * - TODO: Graphics context.
+ */
+function GridPage() {
+  return <h1>Hello, Test</h1>;
+}
+
+export default GridPage;
---
+++ app/safari/page.tsx
@@ -0,0 +1,10 @@
+/**
+ * Demonstrate common problems with Safari and how to fix them.
+ * - TODO: after opacity animation without will-change, Safari drops content.
+ * @returns
+ */
+function SafariPage() {
+  return <h1>Hello, Next.js!</h1>;
+}
+
+export default SafariPage;
