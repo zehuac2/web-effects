615eaca101c36269fda79875a04a70744de39fc4
Zehua Chen <zehua-chen@outlook.com>
2025-03-15 13:52:32 -0700

    [feat] Add one more todos.

---
+++ app/scroll-animations/page.tsx
@@ -0,0 +1,10 @@
+/**
+ * Demonstrate how to do scroll animations.
+ * - TODO: demonstrate the use of animation-timeline
+ * @returns
+ */
+function ScrollAnimations() {
+  return <h1>Hello, Next.js!</h1>;
+}
+
+export default ScrollAnimations;
