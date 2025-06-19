a494f8a738b659b5e90f668b587a01b05119e5e8
Zehua Chen <zehua-chen@outlook.com>
2025-06-18 18:33:32 -0700

    [feat] Get started on the Safari page.

--- app/safari/page.tsx
+++ app/safari/page.tsx
@@ -1,10 +1,36 @@
+import './page.scss';
+
+function Content() {
+  return (
+    <div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+    </div>
+  );
+}
+
+Content.displayName = 'Content';
+
 /**
  * Demonstrate common problems with Safari and how to fix them.
  * - TODO: after opacity animation without will-change, Safari drops content.
  * @returns
  */
 function SafariPage() {
-  return <h1>Hello, Next.js!</h1>;
+  return (
+    <section>
+      <h1>Safari</h1>
+      <section className="SafariPage__without-will-change">
+        <h2>Without will-change</h2>
+        <Content />
+      </section>
+      <section className="SafariPage__with-will-change">
+        <h2>With will-change</h2>
+        <Content />
+      </section>
+    </section>
+  );
 }
+
+SafariPage.displayName = 'SafariPage';
 
 export default SafariPage;
