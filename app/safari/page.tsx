3be6b6dd5d06f8322f65cbeaf2de2a2b4101525c
Zehua Chen <zehua-chen@outlook.com>
2025-06-18 19:41:04 -0700

    [feat] Improve Safari page implementation.

--- app/safari/page.tsx
+++ app/safari/page.tsx
@@ -1,11 +1,18 @@
+import clsx from "clsx";
+
 import "./page.scss";
 
-function Content() {
+interface ContentProps {
+  className?: string;
+}
+
+function Content({ className }: ContentProps) {
   return (
-    <div className="SafariPageContent">
+    <div className={clsx("SafariPageContent", className)}>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
       <button>Some button</button>
       <div className="SafariPageContent__translated">Translated</div>
+      <svg />
     </div>
   );
 }
@@ -15,19 +22,21 @@ Content.displayName = "Content";
 /**
  * Demonstrate common problems with Safari and how to fix them.
  * - TODO: after opacity animation without will-change, Safari drops content.
- * @returns
  */
 function SafariPage() {
   return (
     <section>
       <h1>Safari</h1>
-      <section className="SafariPage__without-will-change">
+      <p>This is in progress.</p>
+      <section>
         <h2>Without will-change</h2>
-        <Content />
+        <p>After the opacity animation, Safari should (but currently does not) drop content.</p>
+        <Content className="SafariPage__without-will-change" />
       </section>
-      <section className="SafariPage__with-will-change">
+      <section>
         <h2>With will-change</h2>
-        <Content />
+        <p>This should work as expected.</p>
+        <Content className="SafariPage__with-will-change" />
       </section>
     </section>
   );
