6205f61b9ee94f95caed530863ca2bff98a54842
Zehua Chen <zehua-chen@outlook.com>
2025-06-19 18:46:00 -0700

    [feat] Setup grid with shader component

---
+++ app/grid/GridWithShader/GridWithShader.tsx
@@ -0,0 +1,16 @@
+/**
+ * TODO: read these arthicles
+ * - https://bgolus.medium.com/the-best-darn-grid-shader-yet-727f9278b9d8
+ * - https://infinitecanvas.cc/guide/lesson-005#texture-based
+ */
+function GridWithShader() {
+  return (
+    <section>
+      <h2>Grid with Shader</h2>
+    </section>
+  );
+}
+
+GridWithShader.displayName = 'GridWithShader';
+
+export default GridWithShader;
