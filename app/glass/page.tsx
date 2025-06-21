c4a38898e0270f34dc5a3103c070000b422c4026
Zehua Chen <31496190+zehuac2@users.noreply.github.com>
2025-06-21 14:04:41 -0700

    [feat] Setup glass page.

---
+++ app/glass/page.tsx
@@ -0,0 +1,22 @@
+/**
+ * TODO:
+ * @summary Demonstrate glass effects.
+ *
+ * # Observations
+ * ## Regular Glass
+ * - (Optional) Frosted glass background
+ * - Typically 1px border
+ * - Dark theme: slightly lighter color on the border than the background
+ *
+ * ## Apple's Liquid Glass
+ * - Some kind of frosted glass effect as the background
+ * - A border that has frosted glass effect.
+ *   - Top left and bottom right corners are extra light.
+ */
+function GlassPage() {
+  return null;
+}
+
+GlassPage.displayName = 'GlassPage';
+
+export default GlassPage;
