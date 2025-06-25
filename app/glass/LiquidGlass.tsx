79a466712bee0af5e3a9a692678a34a3f79dd409
Zehua Chen <31496190+zehuac2@users.noreply.github.com>
2025-06-24 18:23:52 -0700

    Split into two components

---
+++ app/glass/LiquidGlass.tsx
@@ -0,0 +1,18 @@
+/**
+ * Apple's Liquid Glass
+ * - Some kind of frosted glass effect as the background
+ * - A border that has frosted glass effect.
+ *   - Top left and bottom right corners are extra light.
+ */
+function LiquidGlass() {
+  return (
+    <section>
+      <h2>Liquid Glass</h2>
+      <div />
+    </section>
+  );
+}
+
+LiquidGlass.displayName = 'LiquidGlass';
+
+export default LiquidGlass;
---
+++ app/glass/RegularGlass.tsx
@@ -0,0 +1,19 @@
+/**
+ * Regular Glass
+ * - (Optional) Frosted glass background
+ * - Typically 1px border
+ * - Dark theme: slightly lighter color on the border than the background
+ * - Light theme: darker color on the border than the background
+ */
+function RegularGlass() {
+  return (
+    <section>
+      <h2>Regular Glass</h2>
+      <div />
+    </section>
+  );
+}
+
+RegularGlass.displayName = 'RegularGlass';
+
+export default RegularGlass;
