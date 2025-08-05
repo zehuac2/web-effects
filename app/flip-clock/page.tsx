cec0b0fc6146fd77b7281a316309ad92434e1ee6
Zehua Chen <31496190+zehuac2@users.noreply.github.com>
2025-08-04 20:07:58 -0700

    [feat] Setup flip clock page.

---
+++ app/flip-clock/page.tsx
@@ -0,0 +1,12 @@
+/**
+ * TODO: implement flip clock animation.
+ */
+function FlipClockPage() {
+  return (
+    <div>Flip Clock Page</div>
+  );
+}
+
+FlipClockPage.displayName = "FlipClockPage";
+
+export default FlipClockPage;
