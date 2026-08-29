a0568c559f7de2d2adbbcbca33617580c4313ace
Zehua Chen <31496190+zehuac2@users.noreply.github.com>
2026-08-29 15:12:36 -0700

    Setup base url.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,7 @@
 import { defineConfig } from 'astro/config';
 
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://zehuac2.github.io',
+  base: '/web-effects',
+  output: 'static',
+});
